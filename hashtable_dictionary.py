340c5450031b009c464fe2ca22eddfc4ccb267fc
fmlin <frankmenglin@gmail.com>
2020-02-03 23:50:44 -0800

    hashtable

---
+++ hashtable_dictionary.py
@@ -0,0 +1,15 @@
+#Hash table using dictionary example
+
+A=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+
+def H(x):
+    return x%5
+
+Table={}
+for i in A:
+    if H(i) in Table:
+        Table[H(i)]=Table[H(i)]+[i]
+    else:
+        Table[H(i)]=[i]
+
+print(Table)
